7bc3c63bed84dd38182480c90744e57c403b8aef
LohurLaura <lohurlaura@gmail.com>
2023-10-07 11:01:17 +0300

    1st commit

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Person {
+    String name;
+    int age;
+public Person Jane = new Person();
+Jane.name = "Jane";
+Jane.age = 35;
+public void celebrateBirthday();
+age++;
+public void changeName(String newName);
+Jane = newName;
+public String getName();
+return name;
+public int getAge();
+return age;
+}
---
+++ src/test/java/org/example/PersonTest.java
@@ -0,0 +1,19 @@
+package org.example;
+import org.junit.jupiter.api.Test;
+public class PersonTest {
+@Test public void testChangeAgeSuccessful(){
+    public Person Bob = new Person();
+    Bob.name = "Bob";
+    Bob.age = 25;
+    changeName (String newName) = "Mart";
+    Bob.changeName;
+}
+@Test public void testCelebrateBirthday(){
+
+    public Person Charlie = new Person();
+    Charlie.name = "Charlie";
+    Charlie.age = 35;
+    Charlie.celebrateBirthday();
+    return getAge();
+}
+}
